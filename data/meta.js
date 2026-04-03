5467c301178c89fad1ba3692e45ecc7fbabe81d5
nadhira <azzahra.ndhr@gmail.com>
2026-04-02 21:39:56 -0400

    Initial commit: Texas Triangle EV charging gap dashboard

---
+++ data/meta.js
@@ -0,0 +1,107 @@
+const META = {
+  "totalChargers": 629,
+  "totalDeserts": 0,
+  "desertPct": 0.0,
+  "longestGap": 47.5,
+  "longestGapHwy": "I-10",
+  "avgDist": 5.3,
+  "medianDist": 1.8,
+  "totalTruckStops": 140,
+  "byHighway": {
+    "I-10": {
+      "points": 234,
+      "deserts": 0,
+      "maxGap": 47.5,
+      "avgDist": 10.9,
+      "medianDist": 5.2
+    },
+    "I-35": {
+      "points": 228,
+      "deserts": 0,
+      "maxGap": 27.5,
+      "avgDist": 3.6,
+      "medianDist": 1.9
+    },
+    "I-45": {
+      "points": 288,
+      "deserts": 0,
+      "maxGap": 13.2,
+      "avgDist": 2.0,
+      "medianDist": 1.4
+    }
+  },
+  "histBins": [
+    {
+      "label": "0\u20135",
+      "min": 0,
+      "max": 5,
+      "I-10": 116,
+      "I-35": 169,
+      "I-45": 263,
+      "total": 548
+    },
+    {
+      "label": "5\u201310",
+      "min": 5,
+      "max": 10,
+      "I-10": 31,
+      "I-35": 42,
+      "I-45": 18,
+      "total": 91
+    },
+    {
+      "label": "10\u201315",
+      "min": 10,
+      "max": 15,
+      "I-10": 22,
+      "I-35": 11,
+      "I-45": 7,
+      "total": 40
+    },
+    {
+      "label": "15\u201325",
+      "min": 15,
+      "max": 25,
+      "I-10": 32,
+      "I-35": 5,
+      "I-45": 0,
+      "total": 37
+    },
+    {
+      "label": "25\u201350",
+      "min": 25,
+      "max": 50,
+      "I-10": 33,
+      "I-35": 1,
+      "I-45": 0,
+      "total": 34
+    },
+    {
+      "label": "50\u2013100",
+      "min": 50,
+      "max": 100,
+      "I-10": 0,
+      "I-35": 0,
+      "I-45": 0,
+      "total": 0
+    },
+    {
+      "label": "100+",
+      "min": 100,
+      "max": 9999,
+      "I-10": 0,
+      "I-35": 0,
+      "I-45": 0,
+      "total": 0
+    }
+  ],
+  "networkCounts": {
+    "Tesla": 146,
+    "eVgo Network": 81,
+    "ChargePoint Network": 117,
+    "Electrify America": 38,
+    "Blink Network": 21,
+    "SHELL_RECHARGE": 28,
+    "Other": 198
+  }
+};
